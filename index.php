1fbf72fc26b3a9823882b359f13411eaa0afac51
Astrid Varga <astrid.varga@gmx.de>
2019-02-24 20:25:38 +0100

    initial commit

---
+++ index.php
@@ -0,0 +1,25 @@
+<?php
+
+$animals = [
+    'Tiger',
+    'Dog',
+    'Lion',
+    'Mouse',
+    'Cat'
+];
+
+
+
+// // Iterate over array and display each item
+
+// $names = [
+// 'Mary',
+// 'John',
+// 'Anne' 
+// ]
+
+// foreach ($names as $name) {
+//     echo $name.', ';
+// }
+
+require 'index.view.php';
